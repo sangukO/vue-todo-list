0bab7300b6055cc4d36ff1f19ba6c4371dad417d
sangukO <woqls1972@gmail.com>
2023-09-21 17:25:07 +0900

    fix : import vue-router 제거

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 
-import VueRouter from "vue-router";
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia).use(VueRouter).mount("#app");
+app.use(pinia).mount("#app");
