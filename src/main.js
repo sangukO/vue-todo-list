e90790678aa9bf3669a05e59ee97c0485824a291
sangukO <woqls1972@gmail.com>
2023-09-21 17:16:49 +0900

    feat : 라우팅 설정 및 json-server 연결

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 
+import VueRouter from "vue-router";
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia).mount("#app");
+app.use(pinia).use(VueRouter).mount("#app");
---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>홈페이지입니다.</h1>
+    </div>
+</template>
+
+<script setup></script>
+
+<style></style>
